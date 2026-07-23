37195cb23fa2a98738d7bbdd3881b8f978c66d0d
nulltemp <nulltemp999@gmail.com>
2026-07-23 22:55:55 +0900

    typescriptへの書き換え

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -8,6 +8,7 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: {
+        x: 0,
         y: 200,
       },
       debug: false,
@@ -20,26 +21,28 @@ const config = {
   },
 };
 
-let player;
-let coins;
+let player: Phaser.Physics.Arcade.Sprite;
+let playerBody: Phaser.Physics.Arcade.Body;
+let coins: Phaser.Physics.Arcade.Group;
 let score = 0;
-let scoreText;
-let cursors;
+let scoreText: Phaser.GameObjects.Text;
+let cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
 const game = new Phaser.Game(config);
 
-function preload() {
+function preload(this: Phaser.Scene) {
   this.load.image("player", "assets/player.png");
   this.load.image("coin", "assets/coin.png");
 }
 
-function create() {
+function create(this: Phaser.Scene) {
   // プレイヤーの作成
   player = this.physics.add.sprite(400, 550, "player");
   this.physics.add.existing(player);
-  player.body.setCollideWorldBounds(true);
-  player.body.allowGravity = false;
-  player.body.setImmovable(true);
+  playerBody = player.body as Phaser.Physics.Arcade.Body;
+  playerBody.setCollideWorldBounds(true);
+  playerBody.allowGravity = false;
+  playerBody.setImmovable(true);
 
   // コインのグループ作成
   coins = this.physics.add.group();
@@ -47,11 +50,11 @@ function create() {
   // スコア表示
   scoreText = this.add.text(16, 16, "Score: 0", {
     fontSize: "32px",
-    fill: "#fff",
+    color: "#fff",
   });
 
   // カーソルキーの割り当て
-  cursors = this.input.keyboard.createCursorKeys();
+  cursors = this.input.keyboard!.createCursorKeys();
 
   // コインを生成するタイマーイベント
   this.time.addEvent({
@@ -62,21 +65,21 @@ function create() {
   });
 
   // プレイヤーとコインの衝突判定
-  this.physics.add.overlap(player, coins, collectCoin, null, this);
+  this.physics.add.overlap(player, coins, collectCoin, undefined, this);
 }
 
 function update() {
   // プレイヤーの移動
   if (cursors.left.isDown) {
-    player.body.setVelocityX(-300);
+    playerBody.setVelocityX(-300);
   } else if (cursors.right.isDown) {
-    player.body.setVelocityX(300);
+    playerBody.setVelocityX(300);
   } else {
-    player.body.setVelocityX(0);
+    playerBody.setVelocityX(0);
   }
 
   // 画面下部に到達したコインを削除し、新しいコインを生成
-  const coinArray = coins.getChildren();
+  const coinArray = coins.getChildren() as Phaser.Physics.Arcade.Sprite[];
   for (let i = coinArray.length - 1; i >= 0; i--) {
     const coin = coinArray[i];
     if (coin.y > 600) {
@@ -87,14 +90,19 @@ function update() {
 
 function generateCoin() {
   const x = Phaser.Math.Between(50, 750); // ランダムなX座標
-  const coin = coins.create(x, 0, "coin");
-  coin.body.setCollideWorldBounds(false);
-  coin.body.setGravityY(200);
+  const coin = coins.create(x, 0, "coin") as Phaser.Physics.Arcade.Sprite;
+  const coinBody = coin.body as Phaser.Physics.Arcade.Body;
+  coinBody.setCollideWorldBounds(false);
+  coinBody.setGravityY(200);
 }
 
-function collectCoin(player, coin) {
+function collectCoin(
+  _player: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Physics.Arcade.Body | Phaser.Physics.Arcade.StaticBody | Phaser.Tilemaps.Tile,
+  coinObj: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Physics.Arcade.Body | Phaser.Physics.Arcade.StaticBody | Phaser.Tilemaps.Tile
+) {
+  const coin = coinObj as Phaser.Physics.Arcade.Sprite;
   coin.destroy();
   score += 1;
   scoreText.setText("Score: " + score);
-  generateCoin.call(this); // 新しいコインを生成
+  generateCoin(); // 新しいコインを生成
 }
